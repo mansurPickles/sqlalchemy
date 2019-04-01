ef9cabe4d7e81e911753c4af48552ff751d5be3d
Monsieur Pickles <brandon.luong@csu.fullerton.edu>
2019-04-01 15:53:27 -0700

    added layout template

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Brandon Luong',
+        'title': 'Hello World',
+        'content': 'Hello World!',
+        'data_posted': 'April 01 2019'
+    },
+    {
+        'author': 'Alice',
+        'title': 'Second Post',
+        'content': 'Lorem Ipsom',
+        'data_posted': 'April 02 2019'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page!</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title = "About")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
